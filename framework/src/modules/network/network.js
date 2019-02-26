d4cc8d36f588ad3a6ae34bb893cf304f3f72ce94
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-02-26 18:39:13 +0100

    WIP

--- framework/src/modules/network/network.js
+++ framework/src/modules/network/network.js
@@ -1,4 +1,5 @@
 const P2P = require('@liskhq/lisk-p2p').P2P;
+const { createSystemComponent } = require('../../components/system');
 
 /**
  * Network Module
@@ -11,28 +12,64 @@ module.exports = class Network {
 		this.channel = channel;
 		this.options = options;
 		this.logger = null;
-		this.p2p = new P2P({
-			blacklistedPeers: options.blacklistedPeers || [],
-			connectTimeout: options.connectTimeout === undefined ?
-				5000 : options.connectTimeout,
-			seedPeers: options.seedPeers || [],
-			wsEngine: options.wsEngine || 'ws',
-			nodeInfo: {
-				wsPort: options.wsPort || 5000,
-				nethash:
-					options.nethash,
-				version: options.version,
-				os: options.os,
-				height: 0,
-				options: {
-					broadhash: options.nethash, // Initially, we just set the broadhash to nethash.
-					nonce: options.nonce,
-				},
-			},
-		});
 	}
 
 	async bootstrap() {
+		const loggerConfig = await this.channel.invoke(
+			'lisk:getComponentConfig',
+			'logger'
+		);
+		const storageConfig = await this.channel.invoke(
+			'lisk:getComponentConfig',
+			'storage'
+		);
+		const systemConfig = await this.channel.invoke(
+			'lisk:getComponentConfig',
+			'system'
+		);
+
+		this.logger = createLoggerComponent(loggerConfig);
+		const dbLogger =
+			storageConfig.logFileName &&
+			storageConfig.logFileName === loggerConfig.logFileName
+				? this.logger
+				: createLoggerComponent(
+						Object.assign({}, loggerConfig, {
+							logFileName: storageConfig.logFileName,
+						})
+					);
+
+		try {
+			// Storage
+			this.logger.debug('Initiating storage...');
+			const storage = createStorageComponent(storageConfig, dbLogger);
+
+			// System
+			this.logger.debug('Initiating system...');
+			const system = createSystemComponent(systemConfig, this.logger, storage);
+
+			let p2pConfig = {
+				...this.options,
+
+			};
+
+			this.p2p = new P2P({
+				blacklistedPeers: options.blacklistedPeers || [],
+				connectTimeout: options.connectTimeout === undefined ?
+					5000 : options.connectTimeout,
+				seedPeers: options.seedPeers || [],
+				wsEngine: options.wsEngine || 'ws',
+				nodeInfo: system.headers,
+			});
+
+			await this.p2p.start();
+		} catch (error) {
+			this.logger.fatal('Network initialization', {
+				message: error.message,
+				stack: error.stack,
+			});
+			process.emit('cleanup', error);
+		}
 	}
 
 	async cleanup() {
